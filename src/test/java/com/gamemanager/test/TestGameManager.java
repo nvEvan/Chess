aee56e7735723f165459ca53f5aa50c5424abbe6
nvEvan <evan-west@hotmail.com>
2018-03-25 00:49:56 -0400

    Unit Test placeholder for the file reading function, Comments, and ToDo's

--- src/test/java/com/gamemanager/test/TestGameManager.java
+++ src/test/java/com/gamemanager/test/TestGameManager.java
@@ -13,20 +13,32 @@ import org.testng.annotations.Test;
  */
 public class TestGameManager {
 	
+	
+	//To-Do: Research commenting practices for Unit Functions
 	/**
 	 * Unit Test for the <code>getBoard</code> function. 
 	 * 
-	 * Ensures that a deep copy of the <code>GameManager</code> object's board 
+	 * Ensures that a deep copy of the board from the <code>GameManager</code> instance
 	 * is returned.
 	 */
 	@Test
 	public void testGetBoard() {
-		
+		fail("Not yet implemented");
 	}
 	
-	//Set dependancy for this test to the testGetBoard() test
 	/**
-	 * Unit Test for the no-arguments constructor
+	 * Unit Test for the 
+	 */
+	@Test
+	public void testReadAndParseAndCheckFile() {
+		fail("Not yet implemented");
+	}
+	
+	//Set dependancy for this test to the testGetBoard() and testReadAndParseAndCheckFile() tests
+	/**
+	 * Unit Test for the no-arguments constructor.
+	 * 
+	 * Ensures that the default file is read and interpreted properly.
 	 */
 	@Test
 	public void testNoArgsConstructor() {
@@ -34,10 +46,16 @@ public class TestGameManager {
 	}
 	
 	/**
-	 * Unit Test for the constructor taking a filename
+	 * Unit Test for the constructor that takes a filename.
+	 * 
+	 * Ensures that the a custom board state can be read and stored. 
+	 * Also ensures that exceptions are thrown on files that don't exist
+	 * and on files with invalid content. 
 	 */
 	@Test
 	public void testFilenameConstructor() {
 		fail("Not yet implemented");
 	}
+	
+	//ToDo: Test Driven Development -> Write tests for the rest of the GameManager functionality
 }
