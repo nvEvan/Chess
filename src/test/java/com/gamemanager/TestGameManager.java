58eb249e18f3c1a1107b28f92c69a8e823602355
nvEvan <evan-west@hotmail.com>
2018-03-24 22:32:38 -0400

    Create file for testing the GameManager class

---
+++ src/test/java/com/gamemanager/TestGameManager.java
@@ -0,0 +1,14 @@
+package com.gamemanager;
+
+import static org.junit.Assert.*;
+
+import org.testng.annotations.Test;
+
+public class TestGameManager {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
